aa3cc46fbeb4d56a5136d9a4ee928576007c389d
OTooleMichael <otoole.timmy@gmail.com>
2017-10-08 00:29:04 +0100

    Readable Stream

--- test.js
+++ test.js
@@ -1,18 +1,28 @@
-const NVM = require('./NVM');
+const NVM = require('./index');
 const CONFIG = require('./config');
 const moment = require('moment');
 
 
 function start()
 {
+	let i = 0;
 	var nmv = new NVM(CONFIG);
-	let report = nmv.newReport({
+	let report = new nmv.Report({
 		report:"INBOUND",
 		start:'2017-06-01',
-		end:'2017-06-04'
+		end:'2017-06-15'
 	})
+	.limit(400)
+	/*
+	.run(function(err,result){
+		console.log(result);
+		console.log(result.rows.length);
+	})
+	*/
+	.stream()
 	.on('data',function(row){
 		console.log(row);
+		console.log(i++);
 	})
 	.on('error',function(err){
 		console.log(err)
@@ -21,6 +31,6 @@ function start()
 		console.log("ALL done");
 		console.log(this.results);
 	})
-	.stream();
+	;
 }
 start();
